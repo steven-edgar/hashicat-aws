be17925119c1fa9c0a00448b3874acd1b99e4ad0
Steven Edgar <40696015+steven-edgar@users.noreply.github.com>
2020-07-02 12:53:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource aws_instance "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "${var.prefix}-hashicat-instance"
+    Name = "${var.prefix}-hashicat2-instance"
     Billable = "true"
     Department = "devops"
   }
